36ca8f06c5c3c4afb768c4fc9084d0baa21c52b7
Alexy Lafosse <149681987+XLA17@users.noreply.github.com>
2025-12-03 18:25:14 +0100

    add: a* algo

--- Assets/Scripts/Algos/AStar.cs
+++ Assets/Scripts/Algos/AStar.cs
@@ -1,59 +1,227 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
-public class AStarBox
+//public class AStarBox
+//{
+//    private Vector2 position;
+//    private int value;
+//    private int weight;
+
+//    public AStarBox(Vector2 position, int value, int weight)
+//    {
+//        this.position = position;
+//        this.value = value;
+//        this.weight = weight;
+//    }
+//}
+
+//public class AStar
+//{
+//    private List<AStarBox> map;
+//    private int widthMap;
+//    private int heightMap;
+//    private Vector2 startPos;
+//    private Vector2 endPos;
+
+//    public AStar(List<AStarBox> map, int widthMap, int heightMap, Vector2 startPos, Vector2 endPos)
+//    {
+//        if (map.Count != widthMap *  heightMap)
+//        {
+//            throw new Exception("map length != width * height");
+//        }
+//        this.map = map;
+//        this.widthMap = widthMap;
+//        this.heightMap = heightMap;
+//        this.startPos = startPos;
+//        this.endPos = endPos;
+//    }
+
+//    private int GetDistance(Vector2 position)
+//    {
+//        return (int)Mathf.Max(Mathf.Abs(endPos.x - position.x), Mathf.Abs(endPos.y - position.y));
+//    }
+
+//    public void Resolution()
+//    {
+//        List<AStarBox> boxesToCheck = new List<AStarBox>();
+
+
+//    }
+
+//    public void Recursif(Vector2 currentPos)
+//    {
+//        int distance = GetDistance(currentPos);
+
+
+//    }
+//}
+
+
+public struct TileInfos
 {
-    private Vector2 position;
-    private int value;
-    private int weight;
+    public Vector2? parent;
+    public int value;
 
-    public AStarBox(Vector2 position, int value, int weight)
+    public TileInfos(Vector2? parent, int value)
     {
-        this.position = position;
+        this.parent = parent;
         this.value = value;
-        this.weight = weight;
     }
 }
 
 public class AStar
 {
-    private List<AStarBox> map;
-    private int widthMap;
-    private int heightMap;
-    private Vector2 startPos;
-    private Vector2 endPos;
+    //[SetUp]
 
-    public AStar(List<AStarBox> map, int widthMap, int heightMap, Vector2 startPos, Vector2 endPos)
+    private static void PrintList(List<Vector2> list)
     {
-        if (map.Count != widthMap *  heightMap)
+        foreach (Vector2 vector in list)
         {
-            throw new Exception("map length != width * height");
+            Debug.Log($"Vector2: X = {vector.x}, Y = {vector.y}");
         }
-        this.map = map;
-        this.widthMap = widthMap;
-        this.heightMap = heightMap;
-        this.startPos = startPos;
-        this.endPos = endPos;
     }
 
-    private int GetDistance(Vector2 position)
+    private static void PrintList(List<int> list)
     {
-        return (int)Mathf.Max(Mathf.Abs(endPos.x - position.x), Mathf.Abs(endPos.y - position.y));
+        foreach (int elem in list)
+        {
+            Debug.Log($"int: {elem}");
+        }
     }
 
-    public void Resolution()
+    public static void PrintList(Dictionary<Vector2, TileInfos> dictionary)
     {
-        List<AStarBox> boxesToCheck = new List<AStarBox>();
-
-        
+        foreach (KeyValuePair<Vector2, TileInfos> entry in dictionary)
+        {
+            Debug.Log($"Position: {entry.Key}, Parent: {entry.Value.parent}, Value: {entry.Value.value}");
+        }
     }
 
-    public void Recursif(Vector2 currentPos)
+    public static void Test(Tilemap[] tilemaps)
     {
-        int distance = GetDistance(currentPos);
+        var list = Compute(tilemaps, new Vector2(-35, -19), new Vector2(-21, -5));
+        //List<Vector2> list = Compute(startPosition, graphWidth, graphHeight, startIndex, endIndex);
+        //PrintList(list);
 
+        Debug.Log("test");
 
+    }
+
+    public static KeyValuePair<Vector2, TileInfos> GetMinValueEntry(Dictionary<Vector2, TileInfos> dictionary)
+    {
+        var minEntry = new KeyValuePair<Vector2, TileInfos>(default, new TileInfos(null, int.MaxValue));
+        bool first = true;
+
+        foreach (var kvp in dictionary)
+        {
+            if (first || kvp.Value.value < minEntry.Value.value)
+            {
+                minEntry = kvp;
+                first = false;
+            }
+        }
+
+        return minEntry;
+    }
+
+    public static (List<TileInfos>, Dictionary<Vector2, TileInfos>, Dictionary<Vector2, TileInfos>) Compute(Tilemap[] tilemaps, Vector2 startPosition, Vector2 endPosition)
+    {
+        var closedList = new Dictionary<Vector2, TileInfos>();
+        var openList = new Dictionary<Vector2, TileInfos>();
+
+        openList[startPosition] = new TileInfos(null, 0);
+
+        while (openList.Count > 0)
+        {
+            
+            var current = GetMinValueEntry(openList);
+            openList.Remove(current.Key);
+
+            if (current.Key == endPosition)
+            {
+                closedList[current.Key] = current.Value;
+                return (ReconstructPath(closedList, endPosition), closedList, openList);
+            }
+
+            List<Vector2> neighborsPos = GetNeighborsIndex(current.Key);
+
+            foreach (Vector2 neighborPos in neighborsPos)
+            {
+                if (closedList.ContainsKey(neighborPos))
+                    continue;
+
+                if (!HasTile(tilemaps, new Vector3Int((int)neighborPos.x, (int)neighborPos.y, 0)))
+                    continue;
+
+                int newValue = (int)(CalculateDistance(startPosition, neighborPos) + CalculateDistance(neighborPos, endPosition));
+
+                if (!openList.TryGetValue(neighborPos, out TileInfos existingTile) || newValue < existingTile.value)
+                {
+                    openList[neighborPos] = new TileInfos(current.Key, newValue);
+                }
+            }
+
+            closedList[current.Key] = current.Value;
+        }
+
+        // Aucun chemin trouvé
+        return new();
+    }
+
+    public static bool HasTile(Tilemap[] tilemaps, Vector3Int position)
+    {
+        bool res = false;
+        foreach (var tilemap in tilemaps)
+        {
+            TileBase tile = tilemap.GetTile(position);
+            if (tile != null)
+            {
+                return true;
+            }
+        }
+        return res;
+    }
+    private static List<TileInfos> ReconstructPath(Dictionary<Vector2, TileInfos> closedList, Vector2 endPos)
+    {
+        var path = new List<TileInfos>();
+        TileInfos current = closedList[endPos];
+
+        while (current.parent != null)
+        {
+            Debug.Log("test");
+            path.Add(current);
+            //closedList.TryGetValue(current, out TileInfos tileInfosValue);
+            current = closedList[current.parent.Value];
+        }
+
+        path.Reverse();
+        return path;
+    }
+
+    private static float CalculateDistance(Vector2 pos1, Vector2 pos2)
+    {
+        return Mathf.Max(Mathf.Abs(pos1.x - pos2.x), Mathf.Abs(pos1.y - pos2.y));
+    }
+
+    private static List<Vector2> GetNeighborsIndex(Vector2 position)
+    {
+        List<Vector2> neighbors = new();
+        int[] dx = { -1, 0, 1, -1, 1, -1, 0, 1 };
+        int[] dy = { -1, -1, -1, 0, 0, 1, 1, 1 };
+
+        for (int i = 0; i < 8; i++)
+        {
+            float nx = position.x + dx[i];
+            float ny = position.y + dy[i];
+            neighbors.Add(new Vector2(nx, ny));
+        }
+
+        return neighbors;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,10 +11,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject[] spawns;
     [SerializeField] private GameObject unitsParent;
     [SerializeField] private GameObject unitPrefab;
+    [SerializeField] private Tilemap[] tilemaps;
 
     public static GameManager Instance;
 
     private static Dictionary<GameObject, Dictionary<GameObject, float>> graph;
+    private List<TileInfos> list;
+    private Dictionary<Vector2, TileInfos> closedList;
+    private Dictionary<Vector2, TileInfos> openList;
 
     void Awake()
     {
@@ -29,6 +36,10 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        list = new List<TileInfos>();
+        openList = new Dictionary<Vector2, TileInfos>();
+        closedList = new Dictionary<Vector2, TileInfos>();
+
         GameObject unit = Instantiate(unitPrefab, spawns[0].transform);
         if (!unit.TryGetComponent(out Player unitScript))
         {
@@ -42,6 +53,11 @@ public class GameManager : MonoBehaviour
         var path = Dijkstra.GetPath(parent, endNode);
         path.RemoveAt(0);
         unitScript.SetTargets(path);
+
+
+        (list, closedList, openList) = AStar.Compute(tilemaps, new Vector2(-35, -19), new Vector2(-21, -5));
+
+
     }
 
     // Update is called once per frame
@@ -68,5 +84,60 @@ public class GameManager : MonoBehaviour
 
             graph[n] = nextNodes;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (list == null || list.Count < 2)
+            return;
+
+        Gizmos.color = Color.green;
+
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            // Vérifiez que parent n'est pas null
+            if (!list[i].parent.HasValue || !list[i + 1].parent.HasValue)
+                continue;
+
+            Vector3 start = new Vector3(list[i].parent.Value.x + 0.5f, list[i].parent.Value.y + 0.5f, 0);
+            Vector3 end = new Vector3(list[i + 1].parent.Value.x + 0.5f, list[i + 1].parent.Value.y + 0.5f, 0);
+
+            // Dessiner la ligne
+            Gizmos.DrawLine(start, end);
+
+            // Calculer le point milieu
+            Vector3 midPoint = (start + end) / 2;
+
+            // Afficher le texte au milieu
+            Handles.Label(midPoint, list[i].value.ToString());
+        }
+
+        if (closedList == null)
+            return;
+
+        //Gizmos.color = Color.red; // Couleur des carrés
+
+        //foreach (KeyValuePair<Vector2, TileInfos> entry in closedList)
+        //{
+        //    // Convertir Vector2 en Vector3 pour Gizmos.DrawCube
+        //    Vector3 position = new Vector3(entry.Key.x + 0.5f, entry.Key.y + 0.5f, 0);
+
+        //    // Dessiner un cube (carré en 2D) de taille 1.1x1.1
+        //    Gizmos.DrawCube(position, new Vector3(1.1f, 1.1f, 0.1f));
+        //}
+
+        //if (openList == null)
+        //    return;
+
+        //Gizmos.color = Color.blue; // Couleur des carrés
+
+        //foreach (KeyValuePair<Vector2, TileInfos> entry in openList)
+        //{
+        //    // Convertir Vector2 en Vector3 pour Gizmos.DrawCube
+        //    Vector3 position = new Vector3(entry.Key.x + 0.5f, entry.Key.y + 0.5f, 0);
+
+        //    // Dessiner un cube (carré en 2D) de taille 1.1x1.1
+        //    Gizmos.DrawCube(position, new Vector3(1.1f, 1.1f, 0.1f));
+        //}
     }
 }
